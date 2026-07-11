4fc5314a213387fd5f42ab731044fe41cd82c968
kushal267 <patelkushal594@gmail.com>
2026-07-11 16:20:40 +0530

    Initial Smart Task Manager setup with authentication

---
+++ app.py
@@ -0,0 +1,149 @@
+from flask import Flask, render_template
+from flask import request, redirect, session
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+
+from models.user import db, User
+from models.task import Task
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret123"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///app.db'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"].strip()
+        password = request.form["password"].strip()
+
+        print("Entered Email:", email)
+
+        user = User.query.filter_by(
+            email=email
+        ).first()
+
+        print("User:", user)
+
+        if user:
+            print("Stored Hash:", user.password)
+
+            result = check_password_hash(
+                user.password,
+                password
+            )
+
+            print("Password Match:", result)
+
+        if user and check_password_hash(
+                user.password,
+                password):
+
+            print("LOGIN SUCCESS")
+
+            session["user_id"] = user.id
+
+            return redirect("/dashboard")
+
+        print("LOGIN FAILED")
+
+    return render_template("login.html")
+'''@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(
+            email=email
+        ).first()
+
+        if user and check_password_hash(
+                user.password,
+                password):
+
+            session["user_id"] = user.id
+
+            return redirect("/dashboard")
+
+    return render_template("login.html")
+
+'''
+@app.route("/register",  methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        name = request.form["name"].strip()
+        email = request.form["email"].strip()
+        password = request.form["password"].strip()
+
+        print(name, email, password)
+
+        hashed = generate_password_hash(
+            password
+        )
+
+        user = User(
+            name=name,
+            email=email,
+            password=hashed
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        print("User created successfully")
+
+        return redirect("/")
+
+    return render_template(
+        "register.html"
+    )
+
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user_id" not in session:
+        return redirect("/")
+    
+    tasks = Task.query.filter_by(
+        user_id=session["user_id"]
+    ).all()
+
+    return render_template(
+        "dashboard.html",
+        tasks=tasks
+    )
+
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/")
+@app.route("/users")
+def users():
+
+    users = User.query.all()
+
+    for u in users:
+        print(u.id, u.name, u.email, u.password)
+
+    return "Check Terminal"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models/user.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+
+    email = db.Column(db.String(100),
+                      unique=True,
+                      nullable=False)
+
+    password = db.Column(db.String(200),
+                         nullable=False)
